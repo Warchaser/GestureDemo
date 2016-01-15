e4719dfa6155cd10d7d511839ef9a10a15e71109
wu.shb <wu.shb@neusoft.com>
2016-01-15 16:34:49 +0800

    fix # initialize this project Signed-off-by: wu.shb <wu.shb@neusoft.com>

---
+++ app/src/main/java/warchaser/com/gesturedemo/main/MainActivity.java
@@ -0,0 +1,68 @@
+package warchaser.com.gesturedemo.main;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import warchaser.com.gesturedemo.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
+    //    testCompile 'junit:junit:4.12'
+    private GestureDetector mGestureDetector;
+    private int mVerticalMinDistance;
+    private int mMinVelocity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mGestureDetector = new GestureDetector((GestureDetector.OnGestureListener) this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+}
